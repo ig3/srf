c219b593b64ebd3043d10eec1ed140149e99d5ea
Ian Goodacre <Ian.Goodacre@entrain.co.nz>
2021-12-31 15:37:48 +1300

    Add time next 24 hours to status on home and front pages

--- bin/cmd.js
+++ bin/cmd.js
@@ -257,10 +257,12 @@ function runServer (opts, args) {
       fields.FrontSide = card.front;
       card.back = srf.render(template.back, fields);
       const statsPast24Hours = srf.getStatsPast24Hours();
+      const statsNext24Hours = srf.getStatsNext24Hours();
       const config = srf.getConfig();
       const ratio = statsPast24Hours.time / config.studyTimeLimit;
       const mode = (ratio > 2) ? 'stop' : (ratio > 1.5) ? 'slow' : 'go';
       statsPast24Hours.time = Math.floor(statsPast24Hours.time / 60);
+      statsNext24Hours.time = Math.floor(statsNext24Hours.time / 60);
       res.render('front', {
         card: card,
         front: card.front,
@@ -269,6 +271,7 @@ function runServer (opts, args) {
         mode: mode,
         theme: config.theme,
         statsPast24Hours: statsPast24Hours,
+        statsNext24Hours: statsNext24Hours,
         maxViewTime: config.maxViewTime
       });
     } else {
